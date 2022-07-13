bc03ca7d4f779a476670f863a5d9fe65c4009fcf
Siponek <szinkowicz@gmail.com>
2022-07-13 18:01:45 +0200

    Aded script to automate potree

--- potreeAutomate.ps1
+++ potreeAutomate.ps1
@@ -1,6 +1,17 @@
-for ($i = 0; $i -le 15; $i++) {
+for ($i = 0; $i -le 26; $i++) {
+    $number = $i.ToString()
     Write-Host "Runnig test $i"
-    ./PotreeConverter.exe .\testInputs\output_1.las -o C:/xampp/htdocs/potree --generate-page boundedCloud_1
+    ./PotreeConverter.exe ".\testInputs\output_1"+ $number + ".las" -o C:/xampp/htdocs/potree --generate-page ("boundedCloud_1_" + $number) 
+}
+for ($i = 0; $i -le 26; $i++) {
+    $number = $i.ToString()
+    Write-Host "Runnig test $i"
+    ./PotreeConverter.exe ".\testInputs\output_2" + $number + ".las" -o C:/xampp/htdocs/potree --generate-page ("boundedCloud_2_" + $number)
+}
+for ($i = 0; $i -le 26; $i++) {
+    Write-Host "Runnig test $i"
+    $number = $i.ToString()
+    ./PotreeConverter.exe ".\testInputs\output_3" + $number + ".las" -o C:/xampp/htdocs/potree --generate-page ("boundedCloud_3_" + $number)
 }
 
 
